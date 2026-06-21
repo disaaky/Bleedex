8d83a0059e2b21dc09762fb05113916c023c6c57
disaaky <dorottya.faa@gmail.com>
2026-06-21 18:02:07 +0200

    Initial commit

---
+++ res/bleedex.js
@@ -0,0 +1,22 @@
+
+
+
+<script>
+	console.log("Usage: type: single/double")
+
+	function n(type, size, witdh, height, bleed)
+	{
+		if(type="single")
+			
+		if else(type="double")
+		{
+			
+		}
+		
+		else{
+				console.log('Please enter a valid project type')
+			
+		}
+	}
+
+</script>
